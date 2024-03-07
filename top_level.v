fb808fb4aa7c8f9eada37431cc60a4e637d6b0a6
atefehmk <102993614+atefehmk@users.noreply.github.com>
2024-03-07 23:06:46 +0330

    Add files via upload

---
+++ top_level.v
@@ -0,0 +1,38 @@
+module TopLevel(
+    input clock, rst
+    );
+	 
+	 
+    wire clk;
+    assign clk = clock;
+
+    
+    // IF
+    wire [31:0] pcOutIf, instOutIf;
+    // IF-ID
+    wire [31:0] pcOutIfId, instOutIfId;
+
+	 
+	// IF stage  
+	StageIf stageIf(
+    .clk(clock), .rst(rst),
+    .branchTaken(1'b0),
+	 .freeze(1'b0),
+    .branchAddr(32'b0),
+    .pc(pcOutIf),
+	 .instruction(instOutIf)
+	);
+	
+	// regs between If & Id
+	RegsIfId regsIf(
+		.clk(clock), .rst(rst),
+		.freeze(1'b0),
+		.flush(1'b1),
+		.pcIn(pcOutif), .instructionIn(instoutIf),
+		.pcOut(pcOutIfId) , .instructionOut(instOutIfId)
+	
+	);
+	
+	
+	 
+endmodule
